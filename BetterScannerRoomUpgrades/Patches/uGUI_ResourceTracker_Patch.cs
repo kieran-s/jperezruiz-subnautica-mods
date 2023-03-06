1cb0792f6baa1093f4be021523fa587ccf1bac93
Javier <jperezruiz193@gmail.com>
2023-03-06 19:04:39 +0100

    fix(BetterScannerRoomUpgrades): ui blips disappear at 500m regardless the range upgrades

---
+++ BetterScannerRoomUpgrades/Patches/uGUI_ResourceTracker_Patch.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using BetterScannerRoomUpgrades.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BetterScannerRoomUpgrades.Patches
+{
+    /**
+     * Patch uGUI_ResourceTracker in charge of rendering the blips for
+     * the scanned resources
+     */
+    [HarmonyPatch(typeof(uGUI_ResourceTracker))]
+    public class uGUI_ResourceTracker_Patch
+    {
+        [HarmonyPatch(nameof(uGUI_ResourceTracker.GatherAll))]
+        [HarmonyPrefix]
+        public static bool GatherAll(uGUI_ResourceTracker __instance)
+        {
+            var camera = MainCamera.camera;
+            __instance.nodes.Clear();
+            __instance.techTypes.Clear();
+            ResourceTrackerDatabase.GetTechTypesInRange(camera.transform.position, SMLConfig.MaxRange, (ICollection<TechType>) __instance.techTypes);
+            for (int index = 0; index < __instance.techTypes.Count; ++index)
+            {
+                TechType techType = __instance.techTypes[index];
+                ResourceTrackerDatabase.GetNodes(camera.transform.position, SMLConfig.MaxRange, techType, (ICollection<ResourceTrackerDatabase.ResourceInfo>) __instance.nodes);
+            }
+
+            return false;
+        }
+
+        [HarmonyPatch(nameof(uGUI_ResourceTracker.GatherScanned))]
+        [HarmonyPrefix]
+        public static bool GatherScanned(uGUI_ResourceTracker __instance)
+        {
+            Camera camera = MainCamera.camera;
+            __instance.nodes.Clear();
+            __instance.mapRooms.Clear();
+            MapRoomScreen screen = uGUI_CameraDrone.main.GetScreen();
+            if ((UnityEngine.Object) screen != (UnityEngine.Object) null)
+                __instance.mapRooms.Add(screen.mapRoomFunctionality);
+            else
+                MapRoomFunctionality.GetMapRoomsInRange(camera.transform.position, SMLConfig.MaxRange, (ICollection<MapRoomFunctionality>) __instance.mapRooms);
+            for (int index = 0; index < __instance.mapRooms.Count; ++index)
+            {
+                if (__instance.mapRooms[index].GetActiveTechType() != TechType.None)
+                    __instance.mapRooms[index].GetDiscoveredNodes((ICollection<ResourceTrackerDatabase.ResourceInfo>) __instance.nodes);
+            }
+
+            return false;
+        }
+    }
+}
